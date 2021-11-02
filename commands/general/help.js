f891c017a22342d69e3ad42f457be5808287fad3
Xenotic <74404355+IntelligentZero@users.noreply.github.com>
2021-11-02 20:49:33 +0300

    Update help.js
    Embed form

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,6 +1,14 @@
 module.exports = {
   name: "help",
   execute(args, client, msg) {
-    client.createMessage(msg.channel.id, "```setautorole - Set an auto role\nsetmodlogs - Set mod logs channel\nhelp - Show this menu\nping - Show client latency\ncalculate - Calculate\nserverinfo- Show server informations\nuserinfo- Show an user informations```")
-  }
-};
+    client.createMessage(msg.channel.id, {embeds: [
+      {
+       
+        title: 'Help Page!',
+                description: 'Autorole:\nsetautorole - Set an auto role\nLogging:\nsetmodlogs - Set mod logs channel\nGeneral:\nhelp - Show this menu\nping - Show client latency\nUtility:\ncalculate - Calculate\nserverinfo - Show server informations\nuserinfo - Show user information\ntoken - Shows the start of the user\'s token. (User ID in base64. Will be incorrect if user has 2FA.)\nBot:\nprivacypolicy - Shows the privacypolicy of the bot.',
+                color: client.color()
+
+      }
+    ]
+    })
+}};
